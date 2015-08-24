0160e5b083354fec9f3b491468a1c6b4aaa140b6
youssef harkati <youss.hark@gmail.com>
2015-08-24 16:16:57 +0200

    test 3

--- SimulateurCredit/src/metier/TestMetier.java
+++ SimulateurCredit/src/metier/TestMetier.java
@@ -3,7 +3,7 @@ package metier;
 public class TestMetier {
 	
 	public static void main(String[] args) {
-		//test du simulateur 
+		//test du simulateur 2
 		CreditMetier metier = new CreditMetier();
 		double m = metier.calculMensualite(200000, 240, 4.5);
 		System.out.println(m);
